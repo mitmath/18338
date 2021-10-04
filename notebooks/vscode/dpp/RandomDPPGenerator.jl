593b72fd4141ef1b192acc3dbfbce90a44bfe21f
Alan Edelman <edelman.alan@gmail.com>
2021-10-04 07:23:23 -0400

    update

---
+++ notebooks/vscode/dpp/RandomDPPGenerator.jl
@@ -0,0 +1,35 @@
+using LinearAlgebra, Statistics
+
+HH(x) = (w = x + norm(x)*[1;zeros(length(x)-1)]; I-2w*w'/w'w)
+
+function randprojDPP(Y)
+    n = size(Y,2)
+    𝓘 = fill(0,n)
+    for k=1:n
+        p = sum(Y.^2, dims=2)/(n-k+1)
+        𝓘[k] = rand( Categorical(p[:]))
+        Y *=  HH( Y[𝓘[k],:])[:,2:end] 
+    end
+    return(sort(𝓘))
+end
+
+function randDPP(Y,Λ)
+    mask = rand.(Bernoulli.(Λ./(Λ.+1)))
+    return(randprojDPP(Y[:,mask]))
+end
+
+### test--------------------------------------
+N = 4
+Λ = rand(N)
+Y = Matrix(qr(randn(N,N)).Q)
+L = Y * diagm(Λ) * Y'
+
+hist = Dict( 𝓘=>0 for 𝓘∈powerset(1:N) )
+t = 100_000
+for i=1:t
+    hist[randDPP(Y,Λ)] += 1
+end
+hist
+for 𝓘∈powerset(1:N)
+  println(𝓘, " ", hist[𝓘]/t," ",round.(det(L[𝓘,𝓘])/det(L+I),digits=5))
+end
--- notebooks/vscode/dpp/RandomProjectionDPP.jl
+++ notebooks/vscode/dpp/RandomProjectionDPP.jl
@@ -8,14 +8,15 @@ K = Y*Y'
 HH(x) = (w = x + norm(x)*[1;zeros(length(x)-1)]; I-2w*w'/w'w)
 ## the above need not be formed
 
-function randDPP(Y)
+function randprojDPP(Y)
     N,n = size(Y)
     𝓘 = Int64[]
     for k=1:n
         p = sum(Y.^2, dims=2)/(n-k+1)
         i = rand( Categorical(p[:]))
-        append!(𝓘,[i])   
-        Y *= HH( Y[i,:])[:,2:end] 
+        append!(𝓘,[i]) 
+        H = HH( Y[i,:])[:,2:end] 
+        Y *= H
     end
     return(sort(𝓘))
 end
@@ -23,15 +24,14 @@ end
 Y = Matrix(qr(randn(N,n)).Q)
 K = Y*Y'
 
-## Do a test
 
+## Do a test
 hist = Dict( 𝓘=>0 for 𝓘∈combinations(1:N,3) )
 t = 100_000
 for i=1:t
-    hist[randDPP(Y)] += 1
+    hist[randprojDPP(Y)] += 1
 end
 hist
-
 for 𝓘∈combinations(1:N,3)
   println(𝓘, " ", hist[𝓘]/t," ",round.(det(K[𝓘,𝓘]),digits=5))
 end
